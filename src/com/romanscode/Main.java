6d4110be40f4f14ee146fdafa027178e0274dd25
Roman1185 <ramaxin@gmail.com>
2019-10-07 11:44:10 -0700

    ghg

--- src/com/romanscode/Main.java
+++ src/com/romanscode/Main.java
@@ -4,12 +4,75 @@ package com.romanscode;
 public class Main {
 
     public static void main(String[] args) {
-       /* for (int i = 0;i<=100; i++) {
+       /* for (int i = 1;i<=100; i++) {
             System.out.println(i);
         }
     }
     }
- */
+
 
 //Task3
+        var a = 0;
+        for (int i = 0; i<=100;i++) {
+            a = a + i;
+        }
+            System.out.println(a);
+        }
+    }
 
+
+       //Task4
+    var a=0;
+    var b=1;
+    var sum=0;
+    while(a <=100) {
+        if (b <= 3) {
+
+            b++;
+            sum = sum + a;
+        }
+        a++;
+    }
+        System.out.println(sum);
+
+        }
+
+    }
+
+
+
+        //Task5
+        var a = 100;
+        var b = 1;
+        var sum = 0;
+        while (a <= 200) {
+            if (b > 97) {
+                sum = sum + a;
+            }
+            b++;
+            a++;
+        }
+        System.out.println(sum);
+    }
+    }
+
+
+       //2 задачі з заняття
+    char c=0;
+    for(int i=0;i<=50;i++) {
+        c = (char) (i +'a'+ c);
+    }
+        System.out.println(c);
+    }
+    }
+
+
+
+        for (int i = 100; i > 0; i--) {
+
+            System.out.println(i);
+        }
+    }
+}
+
+        */
